0a2de061af8d5922c4f3f0d2d2f2130f8f954bce
Artem Kovalev <rrarrk123@gmail.com>
2022-08-22 02:15:40 +0300

    Добавил HTML файл и передал все данные с бд.

--- mydjango/news/urls.py
+++ mydjango/news/urls.py
@@ -4,6 +4,4 @@ from .views import *
 
 urlpatterns = [
     path('',index),
-    path('test/',test),
-
 ]
--- mydjango/news/views.py
+++ mydjango/news/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import News
+
 
 def index(request):
-    return HttpResponse('Hello world')
+    news = News.objects.order_by('-created_at')
+    return render(request,'news/index.html',{'news':news,'title':'Список новостей'})
 
-def test(request):
-    return HttpResponse('<h1>Тестовая страница</h1>')
+
 
 
 
