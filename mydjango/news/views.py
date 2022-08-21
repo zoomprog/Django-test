ad1a913b91822fb53a5a26adc4bc10422be4fcdc
Artem Kovalev <rrarrk123@gmail.com>
2022-08-21 22:55:40 +0300

    Первые тесты на фреймворке django + работы с бд

---
+++ mydjango/news/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+def index(request):
+    return HttpResponse('Hello world')
+
+def test(request):
+    return HttpResponse('<h1>Тестовая страница</h1>')
+
+
+
