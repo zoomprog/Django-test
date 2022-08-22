e4968c01ca4b96b44621896ed2efb6031e9fd5b8
Artem Kovalev <rrarrk123@gmail.com>
2022-08-22 18:49:23 +0300

    Создал модель Category и объединил ее с News. Внес несколько изменений в админку(добавил чекбоксы и фильтры)

--- mydjango/news/admin.py
+++ mydjango/news/admin.py
@@ -1,12 +1,21 @@
 from django.contrib import admin
 
-from .models import News
+from .models import News, Category
 
 
 class NewsAdmin(admin.ModelAdmin):
-    list_display = ('id','title','created_at','updated_at','is_published')
-    list_display_links = ('id','title')#указывает какие модели будут являтся ссылками
-    search_fields = ('title','content')#добовляет поиск в админке
+    list_display = ('id', 'title', 'category', 'created_at', 'updated_at', 'is_published')
+    list_display_links = ('id', 'title')  # указывает какие модели будут являтся ссылками
+    search_fields = ('title', 'content')  # добовляет поиск в админке
+    list_editable = ('is_published',)# добавляет чекбоксы
+    list_filter = ('is_published','category')#добавляет фильтр в админку
 
 
-admin.site.register(News,NewsAdmin)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title')
+    list_display_links = ('id', 'title')  # указывает какие модели будут являтся ссылками
+    search_fields = ('title',)  # добовляет поиск в админке
+
+
+admin.site.register(News, NewsAdmin)
+admin.site.register(Category, CategoryAdmin)
--- mydjango/news/models.py
+++ mydjango/news/models.py
@@ -8,6 +8,7 @@ class News(models.Model):
     updated_at = models.DateTimeField(auto_now=True,verbose_name='Обновленно')
     photo = models.ImageField(upload_to='photos/%Y/%m/%d/',verbose_name='Фото',blank=True)
     is_published = models.BooleanField(default=True,verbose_name='Обновленно?')
+    category = models.ForeignKey('Category', on_delete=models.PROTECT,null=True,verbose_name='Категория')#models.PROTECT обеспечивает защиту от удаления
 
     def __str__(self):
         return self.title  # для вывода факьтческого названия объекта в консоль  вместо Object
@@ -17,3 +18,16 @@ class News(models.Model):
         verbose_name ='Новость'
         verbose_name_plural ='Новости'
         ordering = ['-created_at']
+
+
+class Category(models.Model):
+    title = models.CharField(max_length=150,db_index=True,verbose_name='Наименование категории')
+
+    def __str__(self):
+        return self.title# дает доступ к выбору категорий
+
+
+    class Meta:
+        verbose_name ='Категории'
+        verbose_name_plural ='Категории'
+        ordering = ['title']
