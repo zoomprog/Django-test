9751320b9209e390afc3d28cc508ce2f4f0971d7
Artem Kovalev <rrarrk123@gmail.com>
2022-08-28 18:36:07 +0300

    Improved the form

--- mydjango/news/views.py
+++ mydjango/news/views.py
@@ -1,5 +1,4 @@
-from django.shortcuts import render, get_object_or_404
-
+from django.shortcuts import render, get_object_or_404, redirect
 
 from .models import News,Category
 from .forms import NewsForm
@@ -27,7 +26,11 @@ def view_news(request, news_id):
 
 def add_news(request):
     if request.method == 'POST':
-        pass
+        form = NewsForm(request.POST)
+        if form.is_valid():
+            news = News.objects.create(**form.cleaned_data)
+            return redirect(news)
+
     else:
         form = NewsForm()
     return render(request, 'news/add_news.html', {'form': form})
